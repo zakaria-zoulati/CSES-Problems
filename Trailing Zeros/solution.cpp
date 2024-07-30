f206a73a0e26e2723a6f3727b1277ee7ab332671
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-07-30 15:03:51 +0200

    Done

---
+++ Trailing Zeros/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+using namespace std ; 
+
+int solve(int n)
+{
+    if (n == 0) {
+        return 0;
+    }
+    return n / 5 + solve(n / 5);
+}
+
+int main()
+{
+    int n ; cin >> n ; 
+    cout << solve(n) << endl ; 
+    return 0;
+}
