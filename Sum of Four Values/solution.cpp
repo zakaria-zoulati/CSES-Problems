60106a6523e6bcc0871086099dceae40ae13577e
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-05-04 23:07:52 +0000

    Sorting Pb

---
+++ Sum of Four Values/solution.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<pair<int, int>> arr(n);
+    for (int i = 0; i < n; ++i) {
+        int val;
+        cin >> val;
+        arr[i] = {val, i + 1};
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 3; j < n; ++j) {
+            int target = x - arr[i].first - arr[j].first;
+            int l = i+1, r = j-1;
+            while (l < r) {
+                int sum = arr[l].first + arr[r].first;
+                if (sum == target) {
+                    cout << arr[i].second << " " << arr[j].second << " "
+                         << arr[l].second << " " << arr[r].second << endl;
+                    return 0;
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE" << endl;
+    return 0;
+}
